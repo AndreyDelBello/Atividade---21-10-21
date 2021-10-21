c228f953ea0c59145a1354ea703d121f8755f7e0
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2021-10-21 19:50:35 +0000

    Arquivos

---
+++ ProgramaPrincipal.java
@@ -0,0 +1,54 @@
+public class ProgramaPrincipal{
+
+    public static void main(String[] args) {
+        
+    AtributoExtensao ay= new AtributoExtensao();
+
+    ay.numero2=10;
+    }
+}
+
+/* Variável = Uma variável é um recurso das linguagens de programação utilizado para armazenar valores em memória.
+Assim, sempre que precisarmos desse valor, basta referenciarmos essa variável. Em Java, podemos
+declarar variáveis, variáveis finais e constantes. As variáveis podem ter seu valor modificado a qualquer momento,
+enquanto as variáveis finais e constantes recebem um valor e não podem ser alteradas.
+Na linguagem Java, declaramos uma variável informando o tipo de dados que ela poderá receber e seu nome.
+
+Exemplos de declaração de variáveis:
+
+int numero;
+String nome;
+
+Neste código informamos que a variável numero é do tipo inteiro e, por isso, só poderá receber valores desse tipo.
+O mesmo comportamento é válido para a variável nome do tipo String.
+
+Definição de Atributo = Atributos são as propriedades de um objeto. Métodos são as ações que um objeto pode realizar.
+Os objetos são características definidas pelas classes. Neles é permitido instanciar objetos da classe para
+inicializar os atributos e invocar os métodos.
+Os atributos também são conhecidos como variáveis de classe, e podem ser divididos em dois tipos básicos:
+atributos de instância e de classe. Os valores dos atributos de instância determinam o estado de cada objeto. 
+Um atributo de classe possui um estado que é compartilhado por todos os objetos de uma classe. Atributos de classe podem
+ser chamados também de atributos estáticos .Constantes são considerados atributos de classe quando estão fora de qualquer método.
+
+
+Objeto = Uma entidade que possui estado e comportamento é conhecida como um objeto, por exemplo, cadeira, bicicleta, marcador,
+caneta, mesa, carro, etc. Pode ser física ou lógica (tangível e intangível). O exemplo de um objeto intangível é o sistema bancário.
+
+Um objeto possui três características:
+
+Estado: representa os dados (valor) de um objeto.
+Comportamento: representa o comportamento (funcionalidade) de um objeto, como depósito, retirada, etc.
+Identidade: uma identidade de objeto geralmente é implementada por meio de um ID exclusivo. O valor do ID não é visível
+para o usuário externo. No entanto, é usado internamente pela JVM para identificar cada objeto exclusivamente.
+
+Um objeto é uma instância de uma classe. Uma classe é um modelo ou blueprint a partir do qual os objetos são criados.
+Portanto, um objeto é a instância (resultado) de uma classe.
+
+Definições de objeto:
+
+Um objeto é uma entidade do mundo real.
+Um objeto é uma entidade de tempo de execução.
+O objeto é uma entidade que possui estado e comportamento.
+O objeto é uma instância de uma classe.
+*/
+
